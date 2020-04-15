ca7ab379d476b7aefc54f0024a3c2fe25f023394
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-15 19:03:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,15 +4,6 @@ def game_hash
       :team_name =>"Brooklyn Nets",
       :colors => [ "Black", "White"],
       :players => [
-        :player_name => ["Alan Anderson", "Reggie Evans", "Brook Lopez"],
-        :number => [0, 30, 11],
-        :shoe => [16, 14, 17],
-        :points => [22, 12, 17],
-        :rebounds => [12, 12, 19],
-        :assists => [12, 12, 10],
-        :steals => [3, 12, 3],
-        :blocks => [1, 12, 1],
-        :slam_dunks => [1, 7, 15]
         ]
     },
     :away => {
@@ -20,15 +11,37 @@ def game_hash
       :colors => [],
       :players =>[
         {
-        :player_name => ,
-        :number => ,
-        :shoe => ,
-        :points => ,
-        :rebounds => ,
-        :assists => ,
-        :steals => ,
-        :blocks => ,
-        :slam_dunks => 
+          :player_name => ,
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        },
+        {
+          :player_name => ,
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        },
+        {
+          :player_name => ,
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
         }
         ]
     }
