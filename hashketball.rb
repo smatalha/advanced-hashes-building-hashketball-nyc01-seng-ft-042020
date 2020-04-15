a5d23535f1f2af1860bdf049e927d2cc9e6c7a09
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-15 19:14:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -75,7 +75,7 @@ def game_hash
           :steals => 4,
           :blocks => 5,
           :slam_dunks => 5
-        }
+        },
         {
           :player_name => "Ben Gordon",
           :number => 8,
