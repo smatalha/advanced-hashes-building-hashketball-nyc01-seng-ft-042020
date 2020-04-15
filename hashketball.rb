e26460bb5b07f1dbba3207cc222a4a669d398e26
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-15 19:03:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,6 +4,39 @@ def game_hash
       :team_name =>"Brooklyn Nets",
       :colors => [ "Black", "White"],
       :players => [
+        {
+          :player_name => ,
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        },
+        {
+          :player_name => ,
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        },
+        {
+          :player_name => ,
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        }
         ]
     },
     :away => {
