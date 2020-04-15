e63d41d6f0c4064e5b9a7b03b8b1209c31934fdd
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-15 19:13:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -78,25 +78,25 @@ def game_hash
         }
         {
           :player_name => "Ben Gordon",
-          :number => 0,
-          :shoe => 16,
-          :points => 12,
-          :rebounds => 4,
-          :assists => 7,
-          :steals => 22,
-          :blocks => 15,
-          :slam_dunks => 10
+          :number => 8,
+          :shoe => 15,
+          :points => 33,
+          :rebounds => 3,
+          :assists => 2,
+          :steals => 1,
+          :blocks => 1,
+          :slam_dunks => 0
         },
         {
-          :player_name => "Bismack Biyombo",
-          :number => 0,
-          :shoe => 16,
-          :points => 12,
-          :rebounds => 4,
-          :assists => 7,
-          :steals => 22,
-          :blocks => 15,
-          :slam_dunks => 10
+          :player_name => "Kemba Walker",
+          :number => 33,
+          :shoe => 15,
+          :points => 6,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 7,
+          :blocks => 5,
+          :slam_dunks => 12
         }
         ]
     }
