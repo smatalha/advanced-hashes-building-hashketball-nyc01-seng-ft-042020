cb98bf9649c870fd9dc0a533b1a263360cc2beeb
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-15 19:11:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -76,6 +76,28 @@ def game_hash
           :blocks => 5,
           :slam_dunks => 5
         }
+        {
+          :player_name => "Bismack Biyombo",
+          :number => 0,
+          :shoe => 16,
+          :points => 12,
+          :rebounds => 4,
+          :assists => 7,
+          :steals => 22,
+          :blocks => 15,
+          :slam_dunks => 10
+        },
+        {
+          :player_name => "Bismack Biyombo",
+          :number => 0,
+          :shoe => 16,
+          :points => 12,
+          :rebounds => 4,
+          :assists => 7,
+          :steals => 22,
+          :blocks => 15,
+          :slam_dunks => 10
+        }
         ]
     }
   }
