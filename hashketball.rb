c16aa012289941b2d9e78d40ddc0734b24dddeec
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-15 19:10:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -45,36 +45,36 @@ def game_hash
       :players =>[
         {
           :player_name => "Jeff Adrien",
-          :number => ,
-          :shoe => ,
-          :points => ,
-          :rebounds => ,
-          :assists => ,
-          :steals => ,
-          :blocks => ,
-          :slam_dunks => 
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2
         },
         {
           :player_name => "Bismack Biyombo",
-          :number => ,
-          :shoe => ,
-          :points => ,
-          :rebounds => ,
-          :assists => ,
-          :steals => ,
-          :blocks => ,
-          :slam_dunks => 
+          :number => 0,
+          :shoe => 16,
+          :points => 12,
+          :rebounds => 4,
+          :assists => 7,
+          :steals => 22,
+          :blocks => 15,
+          :slam_dunks => 10
         },
         {
-          :player_name => "",
-          :number => ,
-          :shoe => ,
-          :points => ,
-          :rebounds => ,
-          :assists => ,
-          :steals => ,
-          :blocks => ,
-          :slam_dunks => 
+          :player_name => "DeSagna Diop",
+          :number => 2,
+          :shoe => 14,
+          :points => 24,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5
         }
         ]
     }
